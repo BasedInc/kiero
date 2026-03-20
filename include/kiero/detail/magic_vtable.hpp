216253cfe9fd15187d2422abc016c216c9fd5ea6
Brady <zeromemesdev@gmail.com>
2026-03-20 10:13:35 -0700

    Update magic_vtable

--- include/kiero/detail/magic_vtable.hpp
+++ include/kiero/detail/magic_vtable.hpp
@@ -22,29 +22,19 @@
 
 #pragma once
 
-#ifndef MAGIC_VTABLE_LIB
-#define MAGIC_VTABLE_LIB
-
 #if defined(_MSC_VER)
-	// Prior to Clang 19.1.0, mangling of functions with NTTP member pointers was incompatible with MSVC 19.20+:
-	// https://releases.llvm.org/19.1.0/tools/clang/docs/ReleaseNotes.html#abi-changes-in-this-version
-	#if (_MSC_VER < 1920) || (defined(__clang__) && __clang_major__ < 19)
-		#define MAGIC_VTABLE_PREFIX std::string_view{"@@$"}
-		#define MAGIC_VTABLE_SUFFIX std::string_view{"@@"}
-	#else
-		#define MAGIC_VTABLE_PREFIX std::string_view{"1@$"}
-		#define MAGIC_VTABLE_SUFFIX std::string_view{"@@"}
-	#endif
+	#define KIERO_MAGIC_VTABLE_PREFIX std::string_view{"@$B"}
+	#define KIERO_MAGIC_VTABLE_SUFFIX std::string_view{"@@"}
 #else
 	#error "Unsupported compiler"
 #endif
 
 #if defined(__cpp_consteval)
-	#define MAGIC_VTABLE_CONSTEVAL consteval
+	#define KIERO_MAGIC_VTABLE_CONSTEVAL consteval
 #elif defined(__cpp_constexpr) && __cpp_constexpr >= 201304L
-	#define MAGIC_VTABLE_CONSTEVAL constexpr
+	#define KIERO_MAGIC_VTABLE_CONSTEVAL constexpr
 #else
-	#define MAGIC_VTABLE_CONSTEVAL inline
+	#define KIERO_MAGIC_VTABLE_CONSTEVAL inline
 #endif
 
 #include <stdexcept>
@@ -57,7 +47,7 @@ namespace magic_vft
 {
 	namespace detail
 	{
-		MAGIC_VTABLE_CONSTEVAL uint8_t parse_alphabet_encoded_hex(const char ch)
+		KIERO_MAGIC_VTABLE_CONSTEVAL uint8_t parse_alphabet_encoded_hex(const char ch)
 		{
 			if (ch < 'A' || ch >= 'A' + 16)
 			{
@@ -66,22 +56,15 @@ namespace magic_vft
 			return static_cast<uint8_t>(ch - 'A');
 		}
 
-		MAGIC_VTABLE_CONSTEVAL size_t decode_microsoft_value(std::string_view str)
+		KIERO_MAGIC_VTABLE_CONSTEVAL size_t decode_microsoft_value(std::string_view str)
 		{
 			using namespace std::literals::string_view_literals;
 
 			// weird cases that i'm not sure how to handle
-			if (str.substr(0, 3) == "B3A"sv)
+			if (str.substr(0, 2) == "3A"sv)
 				return 4;
-			if (str.substr(0, 3) == "B7A"sv)
+			if (str.substr(0, 2) == "7A"sv)
 				return 8;
-
-			// Skip the leading 'B'
-			if (str.front() != 'B')
-			{
-				throw std::invalid_argument{"unexpected character"};
-			}
-			str.remove_prefix(1);
 
 			size_t value{};
 			while (!str.empty() && str.front() != '@')
@@ -95,16 +78,14 @@ namespace magic_vft
 	}
 
 	template<auto>
-	MAGIC_VTABLE_CONSTEVAL size_t vtable_index()
+	KIERO_MAGIC_VTABLE_CONSTEVAL size_t vtable_index()
 	{
 		constexpr std::string_view mangled{__FUNCDNAME__};
-		constexpr auto first = mangled.rfind(MAGIC_VTABLE_PREFIX) + MAGIC_VTABLE_PREFIX.size();
-		constexpr auto last = mangled.rfind(MAGIC_VTABLE_SUFFIX, first);
+		constexpr auto first = mangled.rfind(KIERO_MAGIC_VTABLE_PREFIX) + KIERO_MAGIC_VTABLE_PREFIX.size();
+		constexpr auto last = mangled.rfind(KIERO_MAGIC_VTABLE_SUFFIX, first);
 		constexpr auto value = detail::decode_microsoft_value(mangled.substr(first, last - first));
 		return value / sizeof(size_t);
 	}
 }
 
 } // kiero::detail
-
-#endif // MAGIC_VTABLE_LIB
